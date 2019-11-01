8759f33b472655e480a13ff0bbbf85a060fd117a
League Student <league@iMac-9.attlocal.net>
2019-10-31 18:29:08 -0700

    changes

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -11,7 +11,17 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+		RaceCar rc = new RaceCar(null, 5);
+		System.out.println(rc.getPositionInRace());
+		rc.crash();
+		System.out.println(rc.getPositionInRace());
+		System.out.println(rc.damaged);
+		rc.pit();
+	//	rc.overtake();
+		for (int i = 0; i < 8; i++) {
+			rc.overtake();
+		}
+		
 		// 2. Print the RaceCar's position in the race
 
 		// 3. Crash the RaceCar
